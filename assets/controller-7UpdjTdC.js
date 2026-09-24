567e90ad1a3b2027ba0f1ee18d0167f6439b40a1
JS Zero publisher <nathanshipley@users.noreply.github.com>
2026-09-24 15:59:46 -0400

    Publish JS Zero game build

---
+++ assets/controller-7UpdjTdC.js
@@ -0,0 +1,1 @@
+import{c as t}from"./controllers-GZuwYbAa.js";const r=t();function e(){r.poll(),requestAnimationFrame(e)}requestAnimationFrame(e);
--- assets/controller-BjEwa451.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./controllers-CoorMpMR.js";const r=t();function e(){r.poll(),requestAnimationFrame(e)}requestAnimationFrame(e);
